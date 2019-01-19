8c67233bd6d0a174319e2f8ec478cccab69da1cc
Amit Shukla <amit090shukla@gmail.com>
2019-01-19 14:41:33 +0530

    Problem 1

---
+++ absoluteValueSumMinimization.ts
@@ -0,0 +1,6 @@
+const absoluteValueSumMinimization = (arr: number[]): number => {
+  const isEven = arr.length % 2 === 0;
+  return isEven ? arr[arr.length / 2 - 1] : arr[Math.floor(arr.length / 2)];
+};
+
+console.log(absoluteValueSumMinimization([1, 2, 3, 4, 5]));
